8141afb6e878491a985fd6de0df6479d0d72cf74
Diambombamaria <diambombamaria@gmail.com>
2025-11-02 21:41:19 +0100

    Exercise 3: Implement signUp function for user registration

--- 03-user-management.js
+++ 03-user-management.js
@@ -5,35 +5,34 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
 
-	const utilisateurs = [
+
+
+
+function whoIsAdmin(utilisateurs) {
+  const admins = [];
+
+  for (let i = 0; i < utilisateurs.length; i++) {
+    if (utilisateurs[i].estAdmin === true) {
+      admins.push(utilisateurs[i].nom);
+    }
+  }
+
+  return admins;
+}
+
+
+const utilisateurs = [
   { nom: "Maria", age: 22, estAdmin: true },
   { nom: "Rachel", age: 25, estAdmin: false },
   { nom: "Oracle", age: 19, estAdmin: true }
 ];
 
-function whoIsAdmin(utilisateurs) {
-  const admins = []; 
-
-  for (let i = 0; i < utilisateurs.length; i++) {
-   
-    if (utilisateurs[i].estAdmin === true) {
-      admins.push(utilisateurs[i].nom); 
-    }
-  }
-
-  return admins; 
-}
 
 const resultat = whoIsAdmin(utilisateurs);
+console.log(resultat); 
 
-console.log(resultat);
-	
-}
 
 module.exports = {
-	whoIsAdmin,
+  whoIsAdmin,
 };
-
-
